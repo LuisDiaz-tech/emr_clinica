ce89c64dd48182a946b07344f709f52fdcaf85d6
Luis-TechHome <i4zjojoa@gmail.com>
2026-03-02 18:54:04 -0500

    Implementación de la clase para la conexion PDO

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,20 @@
 <?php
 Class Database{
     private $host ="localhost";
+    private $db ="emr_clinica";
+    private $user = "postgres";
+    private $pass = "Lucas2023.";
+    private $port = "5432";
+
+    public function connect(){
+    try{
+        $pdo = new PDO("pgqsl:host={$this->host};port={$this->port};dbname={$this->db},$this->user,$this->pass");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;    
+        }catch(PDOException $e){
+            die("Error conexión: " . $e->getMessage());
+        }
+    }
 
 
-
-
-    
 }
