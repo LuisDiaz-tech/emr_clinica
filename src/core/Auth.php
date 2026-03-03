51252d9d5a7b08f1284ddc1080a1dd92c10afb19
Luis-TechHome <i4zjojoa@gmail.com>
2026-03-02 22:49:59 -0500

    Ajuste Auth a PSR-4

---
+++ src/core/Auth.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Felipe\EmrClinica\Core;
+
+Class Auth{
+
+    public static function check(){
+        if(!isset($_SESSION['user'])){
+            header("Location: index.php");
+            exit;
+        }
+        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+    }
+
+    public static function role(array $allowedRoles = []){
+        if(!isset($_SESSION['user'])){
+            header("Location: index.php");
+            exit;
+        }
+        if(!in_array($_SESSION['user']['role_name'],$allowedRoles)){
+            echo "Acceso no autorizado";
+            exit;
+        }
+    }
+}
