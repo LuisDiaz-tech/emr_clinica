72ab865cba4422ca57a09d5877c94c11adebefa6
Luis-TechHome <i4zjojoa@gmail.com>
2026-03-02 23:18:13 -0500

    Implementacion de login y logout

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -1,7 +1,31 @@
 <?php
 
+namespace Felipe\EmrClinica\Controllers;
+
 use Felipe\EmrClinica\Models\User;
 use Felipe\EmrClinica\Config\Database;
 
-$db = (new Database())->connect();
-$userModel = new User($db);
+class AuthController{
+    public function login($username, $password){
+    $database = new Database();
+    $pdo = $database->connect();
+
+    $userModel = new User($pdo);
+    $user = $userModel->login($username,$password);
+
+    if($user){
+        $_SESSION['user'] = $user;
+        header("Location: dashboard.php");
+        exit;
+    }
+    return false;
+    }
+
+    public function logout(){
+        session_destroy();
+        header("Location: index.php");
+        exit;
+    }
+
+}
+
