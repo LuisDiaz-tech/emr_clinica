b6bc9b199406bee2ecc1e23fecb17cad244d38e2
Luis-TechHome <i4zjojoa@gmail.com>
2026-03-03 20:51:21 -0500

    Correccion listado buscar pacientes

--- src/Models/Patient.php
+++ src/Models/Patient.php
@@ -26,15 +26,20 @@ class Patient{
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function search($term): array{
-        $sql = "SELECT * FROM pacientes
-        WHERE first_name ILIKE :term
-        OR last_name ILIKE :term 
-        OR document_number ILIKE :term
-        ORDER BY patient_id DESC";
+    public function search(string $term): array{
+        $term = trim($term);
+        $termLike = "%{$term}%";
+
+       $sql = "SELECT * FROM pacientes
+            WHERE first_name ILIKE :term
+            OR last_name ILIKE :term
+            OR document_number ILIKE :term
+            ORDER BY patient_id DESC";
+
     $stmt = $this->conn->prepare($sql);
-    $stmt-> execute(['term' => "%term%"]);
-    
+    $stmt->bindValue(':term', $termLike, \PDO::PARAM_STR);
+    $stmt->execute();
+   
     return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 }
